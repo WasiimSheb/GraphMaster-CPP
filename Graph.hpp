7ecf0fd3725e9f8f4b144061328a3edfc42a72b6
WasiimSheb <123734906+WasiimSheb@users.noreply.github.com>
2024-04-29 23:47:46 +0300

    Create Graph.hpp

---
+++ Graph.hpp
@@ -0,0 +1,19 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+namespace ariel{
+    class Graph{
+        private:
+            vector<vector<int>> adjacencymatrix;
+
+        public:
+            void printGraph();
+            void loadGraph(vector<vector<int>> adjmat);
+            const vector<vector<int>> &getAdjacencyMatrix();
+    };
+}
+#endif // GRAPH_HPP
